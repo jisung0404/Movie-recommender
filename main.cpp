eb0249024297e651fc2363d7b2cdfcb16a82de67
jisung0404 <skhanesa08@gmail.com>
2026-04-19 13:38:00 +0000

    while 무한 반복문 추가 후 종료조건 구현(choice == 1~8 제외 구현)

--- main.cpp
+++ main.cpp
@@ -35,5 +35,25 @@ int main() {
     ratingMgr.addRating(u1.getId(), m1.getId(), 4.4);
     ratingMgr.addRating(u1.getId(), m2.getId(), 6.5);
 
+    int choice;
+
+    while (true) {
+        std::cout << "\n=== Movie Recommender ===\n[영화]\n1. 영화 추가\n2. 제목으로 검색\n3. 전체 목록 출력\n4. 평점순 정렬 출력\n[사용자]\n5. 사용자 추가\n6. 사용자 목록 출력\n[평점]\n7. 평점 입력\n8. 영화별 평점 보기\n0. 종료\n선택 > ";
+        std::cin >> choice;
+
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            std::cout << "잘못된 입력입니다. 숫자를 입력해주세요.\n";
+            continue;
+        }
+
+        if (choice == 0) {
+            std::cout << "프로그램을 종료합니다.\n";
+            break;
+        }
+
+    }
+
     return 0;
 }
