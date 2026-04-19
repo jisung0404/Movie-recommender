d233bbddaa060f71f712746a6d9bcba4873a8756
jisung0404 <skhanesa08@gmail.com>
2026-04-19 12:46:03 +0000

    findByNName 메서드 포인터로 수정(nullptr 반환 목적)

--- UserManager.h
+++ UserManager.h
@@ -11,8 +11,8 @@ private:
 public:
     void addUser(const User& u);
     
-    // 이름으로 찾기
-    User findByName(const std::string& name);
+    // 이름으로 찾기 (못 찾으면 nullptr 반환) 
+    User* findByName(const std::string& name);
     
     // BaseManger의 전체 출력 기능 구현
     void printAll() const override;
