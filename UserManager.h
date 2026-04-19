dad54d48870971e85d6525564f04f560fabf2ef4
jisung0404 <skhanesa08@gmail.com>
2026-04-19 12:44:45 +0000

    UserManger 헤더파일 구현

---
+++ UserManager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "User.h"
+#include "BaseManager.h"
+
+class UserManager : public BaseManager {
+private:
+    std::vector<User> users; // 사용자 목록을 저장
+
+public:
+    void addUser(const User& u);
+    
+    // 이름으로 찾기
+    User findByName(const std::string& name);
+    
+    // BaseManger의 전체 출력 기능 구현
+    void printAll() const override;
+};
