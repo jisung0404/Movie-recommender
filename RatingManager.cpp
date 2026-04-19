dc7b783558549a39d8bcffa32b236ae3fcf1772b
jisung0404 <skhanesa08@gmail.com>
2026-04-19 13:02:58 +0000

    RatingManager.cpp 구현 (평점 추가, 특정 평점 출력, 전체 평점 출력 메소드 구현)

---
+++ RatingManager.cpp
@@ -0,0 +1,28 @@
+#include "RatingManager.h"
+#include <iostream>
+
+using namespace std;
+
+// 1. 평점 추가
+void RatingManager::addRating(int userId, int movieId, double score) {
+    Rating newRating(userId, movieId, score);
+    ratings.push_back(newRating);
+}
+
+// 2. 특정 영화의 평점만 모아서 보기
+void RatingManager::printRatingsByMovie(int targetMovieId) const {
+    bool found = false;
+    for (const Rating& r : ratings) {
+        if (r.getMovieId() == targetMovieId) {
+            cout << r << endl;
+            found = true;
+        }
+    }
+}
+
+// 3. 전체 평점 목록 출력
+void RatingManager::printAll() const {
+    for (const Rating& r : ratings) {
+        cout << r << endl;
+    }
+}
