cfcb9933d6c0d2aa29f3ec3780f144c0b3967efb
jisung0404 <skhanesa08@gmail.com>
2026-04-23 04:16:20 +0000

    display() 삭제

--- User.cpp
+++ User.cpp
@@ -12,13 +12,6 @@ int         User::getId()          const { return id; }
 string      User::getName()        const { return name; }
 string      User::getEmail()       const { return email; }
 
-void        User::display()        const {
-    std::cout << "[" << id 
-              << "] " << name 
-              << " (" << email 
-              << ")" << endl;
-}
-
 // --- [M2 추가] 연산자 오버로딩 구현부 ---
 
 // == 연산자: 이름이 같으면 같은 사용자로 취급 (검색용)
