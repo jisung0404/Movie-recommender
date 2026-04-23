45782fa50ba59ed26e21ac56ea2c7ca30ec9dc5b
jisung0404 <skhanesa08@gmail.com>
2026-04-23 05:00:07 +0000

    테스트 케이스 삭제

--- main.cpp
+++ main.cpp
@@ -14,27 +14,6 @@ int main() {
     UserManager userMgr;
     RatingManager ratingMgr;
 
-    // #1 Movie 생성 및 테스트
-    Movie m1(1, "왕과사는남자", "드라마", 2026);
-    Movie m2(2, "극한직업", "코미디",    2019);
-
-    m1.addRating(4.4);
-    m2.addRating(4.6);
-    m1.addRating(5.0);
-    m1.addRating(6.0);   // 유효성 검사
-
-    movieMgr.addMovie(m1);
-    movieMgr.addMovie(m2);
-
-    // #2 User 생성
-    User u1(1, "김지성", "skhanesa08@gmail.com");
-    
-    userMgr.addUser(u1);
-
-    // #3 Rating 추가
-    ratingMgr.addRating(u1.getId(), m1.getId(), 4.4);
-    ratingMgr.addRating(u1.getId(), m2.getId(), 6.5);
-    
     int choice;
 
     while (true) {
