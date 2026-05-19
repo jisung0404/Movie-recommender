0abade3d2b1500b5ec3dc02b9e2d6cb5731448e7
jisung0404 <skhanesa08@gmail.com>
2026-05-19 10:26:44 +0900

    Refactor: Milestone 3 BaseManager 인터페이스 상속 및 const 시그니처 정문화

--- include/MovieManager.h
+++ include/MovieManager.h
@@ -9,9 +9,12 @@
 class MovieManager : public BaseManager {
 private:
     std::vector<Movie> movies;
+
+    // 교안 12p: 장르 기반 유사도 계산 헬퍼 함수
+    int calculateGenreSimilarity(const std::string& genreA, const std::string& genreB);
+
 public:
     void loadFromFile(const std::string& filename) override;
-    // 뒤에 const를 붙여서 구현부와 상속 구조를 일치시킴
     void saveToFile(const std::string& filename) const override;
     int size() const override;
     
@@ -21,6 +24,9 @@ public:
 
     Movie* findByTitle(const std::string& title);
     void sortByRating();
+
+    // 교안 12p: MovieManager 자체에 위치해야 하는 필수 recommend 메서드
+    std::vector<Movie> recommend(const Movie& targetMovie, int N = 5);
 };
 
 #endif
